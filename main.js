2437df3220feb82974fac06259fe327444de8bd2
fatima awad <fatimamuk0@gmail.com>
2019-01-10 20:02:59 -0500

    First commit of the game

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $("#btn").click(function(){
+        $("#btn").hide();
+        $("#main h1").hide();
+        $("#box").css("visibility","visible");
+        $("#box").animate({width:"700px",
+            height: "430px"},800);
+    })
+
+
+
+
+
+
+})
